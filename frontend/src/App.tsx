19c756c99ec9e59b09f973ccfa13507811fdab5f
Gui Scarme <scarmegui@gmail.com>
2025-11-25 14:48:34 -0300

    Add react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import Header from "./layout/Header";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [msg, setMsg] = useState("");
@@ -10,9 +12,12 @@ function App() {
   }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "2rem" }}>
-      <h1>{msg || "Carregando..."}</h1>
-      <p>Frontend e backend conectados com sucesso 🏀</p>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+      <Header />
+      <div style={{ flex: 1, display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column", padding: "2rem" }}>
+        <h1>{msg || "Carregando..."}</h1>
+        <p>Frontend e backend conectados com sucesso 🏀</p>
+      </div>
     </div>
   );
 }
